8ca1ea74b9a7b44a28acca8800789b66d07ef1b8
aliza-dev <alizait1192@gmail.com>
2025-08-20 00:56:05 +0500

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all  JS code as newer version
+
+alert(3+3)  // we are using nodejs, not browser, so this won't work
+
+console.log( 3+ 3);
+console.log("Aliza");  
+
+let name= "Aliza";
+let age = 20;
+let isLoggedIn = true;
+
+// Primitives data types in JavaScript:
+
+// number=> 2 to power 53
+// bigInt => 2 to power 64
+// string => ""
+// boolean => true/false
+// null => standalone value, empty value, null is a object
+// undefined => variable is declared but not assigned
+// symbol => unique value, we can use in react mostly
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+

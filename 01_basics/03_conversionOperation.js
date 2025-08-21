4d06e65d86a97f83f93ad3f6697faa1bd8f88cf3
aliza-dev <alizait1192@gmail.com>
2025-08-21 20:18:19 +0500

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,54 @@
+let score = "Aliza";
+console.log(typeof (score)); 
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggesdIn = "Aliza";
+let booleanIsLoggedIn = Boolean(isLoggesdIn);
+console.log(booleanIsLoggedIn);
+
+// "Aliza" => true
+// "" => false
+// "0" => true
+// "1" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+// ********************Operations on conversion*****************
+
+let value = 3
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+
+let str1 = "hello";
+let str2 = " Aliza";
+let str3 = str1 + str2;
+console.log(str3); 
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1"+2+2);
+console.log(1+ 2+"2");
+
+console.log((3 + 4) * 5 % 2 );
+
+let gameChanger = 100;
+++gameChanger
+console.log(gameChanger); 

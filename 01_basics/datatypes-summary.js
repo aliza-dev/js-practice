dcb918bcee7f84a5355886948e9e918c36d23d46
aliza-dev <alizait1192@gmail.com>
2025-08-22 18:40:48 +0500

    memory stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -28,3 +28,35 @@ const myFunction = function () {
 }
 
 console.log(typeof bigNumber); 
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "Alizatariqdotcom";
+
+let anotherName = myYoutubeName;
+anotherName = "chaiaurcode";
+
+console.log(anotherName);   // chaiaurcode
+console.log(myYoutubeName); // Alizatariqdotcom
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybi"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "aliza@google.coo"; //  fixed
+
+console.log(userTwo); // { email: 'aliza@google.coo', upi: 'user@ybi' }
+console.log(userOne); // { email: 'aliza@google.coo', upi: 'user@ybi' }
+
+
+
+
+
+
+// Stack : jo bhi variable declare kia ho iska copy milta hai 
+// Heap : iska reference milta hai(jo bhi change karein ghy original value ma change ho ga)
